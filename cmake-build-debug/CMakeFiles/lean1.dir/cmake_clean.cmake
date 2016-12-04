28c5ffdaf17bbfef941bcdc5693240d5d6ef18ee
jack4c <jack4c@jack4cdeMacBook-Pro.local>
2016-12-04 12:25:55 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/lean1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lean1.dir/main.cpp.o"
+  "CMakeFiles/lean1.dir/数据类型.cpp.o"
+  "lean1.pdb"
+  "lean1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lean1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 数据类型.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jack4c on 2016/12/4.
+//
+
+

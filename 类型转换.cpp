1ad921eab6b664e7ee75754d78c87f5a219a3554
jack4c <jack.from.cc@gmail.com>
2016-12-07 00:06:03 +0800

    2.2变量

--- 类型转换.cpp
+++ 类型转换.cpp
@@ -1,13 +1,47 @@
 #include <iostream>
 
 int main() {
-
+/*
     bool b = 42; //b为真
     int i = b;  // i的值为1
-    i = 3.14 ; //i的值为3.0
-    double pi = i ;//
+     double pi = i ;//
+    unsigned char c = -1; // c的值为225
+    signed  char c2 = 257;
+    short s1 = 1 << 16;
+    i =  (i >> 30)  ;
+
 
     std::cout << i << std::endl;
+      std::cout << s1 << std::endl;
+*/
+
+   /* int i = -42;
+    unsigned u = 10;
+    std::cout << i + i << std::endl;
+    std::cout << i + u << std::endl;  // 将u
+*/
+
+    /*unsigned u1 = 20;
+    unsigned u2 = 10;
+
+    std::cout << u1 -u2 <<std::endl;
+    std::cout << u2 -u1 << std::endl;*/
+
+    //32 , 1 << 32 -32 32 -32 0 0
+
+   /* unsigned u = 10,u2 = 42;
+    std::cout << u2 - u << std::endl;
+    std::cout << u - u2 << std::endl;
+
+    int i = 10, i2 = 42;
+    std::cout << i2 - i << std::endl;
+    std::cout << i -i2 << std::endl;
+    std::cout << i - u << std::endl;
+    std::cout << u - i << std::endl;*/
+
+    int month = 07;
+    std::cout << "2M \n2\tM\n"  << std::endl;
+
 
     return 0;
 }
